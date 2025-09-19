4b669f1606f505aeb922a7bd43bde0d4f71d8dc5
Nikola Soric <72814546+nikolasoric@users.noreply.github.com>
2025-09-19 10:05:43 +0200

    Change SKU to Standard

--- terraform/main.tf
+++ terraform/main.tf
@@ -84,6 +84,7 @@ resource "azurerm_public_ip" "public_ip" {
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_network_interface" "nic" {
